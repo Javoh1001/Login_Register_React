d52d3e4a89144994d71bcc29073ec8e490cc1d24
Javohirbek <javoh1001@gmail.com>
2021-11-03 23:26:53 +0500

    all files add

---
+++ src/components/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color:#2a393e;
+    width:800px;
+    height:400px;
+    display:flex;
+    justfiy-content:center;
+    align-items:center;
+`
